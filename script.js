21f4723d9a05980cb048ec5b90e2b9380cd51424
JaysenKar <kartikojaysen72@gmail.com>
2023-04-24 19:27:44 +0800

    commit kedua

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ $(function () {
   });
 });
 
+// const a = document.getElementsByTagName("a");
+// a.forEach((e) => {
+//   e.addEventListener("click", function (el) {
+//     window.scrollTo(window.offsetX - 100);
+//   });
+// });
+
 function myFunction() {
   const nama = document.querySelector("#nama");
   const kelamin = document.querySelector("#jenis-kelamin");
@@ -12,6 +19,9 @@ function myFunction() {
   const komentarDisplay = document.querySelector(".komentar-display");
   const alert = document.querySelector(".alert-success");
   komentarDisplay.innerHTML += `Nama: ${nama.value} <br> Jenis Kelamin: ${kelamin.value} <br> Tanggal Lahir: ${tanggal.value} <br> Komentar: ${komentar.value} <br>`;
+  nama.value = "";
+  tanggal.value = "";
+  komentar.value = "";
   alert.style.display = "block";
   setTimeout(() => {
     alert.style.display = "none";
